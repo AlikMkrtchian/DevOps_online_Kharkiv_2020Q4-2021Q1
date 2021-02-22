5a6371725952d1eb4ae06c6dd2d3079d24030d3b
snnapys <snnapyss1@gmail.com>
2021-02-22 23:51:29 +0200

    fizz

---
+++ module 9/task9/frezz1.py
@@ -0,0 +1,21 @@
+def inputData(): 
+        number = int(input("Enter a Number: ")) 
+        return  number
+
+def fizzbuzz(n):
+        result = []
+        for x in range(1, n+1):
+            if x % 3 == 0 and x % 5 == 0:
+                result.append("fizz buzz")
+            elif x % 3 == 0:
+                result.append('fizz')
+            elif x % 5 == 0:
+                result.append('buzz')
+            else:
+                result.append(str(x))
+        return result
+
+def main():
+        print(fizzbuzz(inputData()))
+
+main()
